4f72331e86b2b5017ed679d4f08911decdfd7db4
Delan Azabani <delan@azabani.com>
2013-02-24 12:26:12 +0800

    calculate memory as total-available to be consistent with percentage on linux

--- static/script.js
+++ static/script.js
@@ -38,7 +38,7 @@
 		c_cpuusage_l.append(+new Date, n);
 	};
 	h.ramusage = function(n) {
-		$('#ramusage').text(u.bytes(n[3]));
+		$('#ramusage').text(u.bytes(n[0] - n[1]));
 		c_ramusage_l.append(+new Date, n[2]);
 	};
 	var count = 0, errors = 0;
